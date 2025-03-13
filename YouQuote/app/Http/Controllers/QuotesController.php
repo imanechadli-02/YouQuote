d8cb471951536229051c08c7370648b33a001b1b
imane <imanechadli2001@gmail.com>
2025-03-13 12:19:23 +0000

    ajout de la fonction random

--- YouQuote/app/Http/Controllers/QuotesController.php
+++ YouQuote/app/Http/Controllers/QuotesController.php
@@ -7,60 +7,44 @@ use Illuminate\Http\Request;
 
 class QuotesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        return Quotes::all();   
+        return Quotes::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate(['content' => 'required|string']);
         return Quotes::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Quotes $quotes)
+    public function show(Quotes $quote)
     {
-        //
+        return $quote;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Quotes $quotes)
+    public function update(Request $request, Quotes $quote)
     {
-        //
+        $quote->update($request->all());
+        return $quote;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Quotes $quotes)
+    public function destroy(Quotes $quote)
     {
-        //
+        $quote->delete();
+        return response()->json(['message' => 'Quote deleted']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Quotes $quotes)
+    public function random()
     {
-        //
+        return Quotes::inRandomOrder()->first();
     }
+
+    public function filterByLength($length)
+    {
+        return Quotes::whereRaw('LENGTH(content) <= ?', [$length])->get();
+    }
+
+    // public function popular() {}
 }
--- YouQuote/routes/api.php
+++ YouQuote/routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('quotes', QuotesController::class);
+Route::apiResource('quotes', QuotesController::class);
+Route::get('./quotes/random', [QuotesController::class, 'random']);
+Route::get('quotes/filter/{length}', [QuotesController::class, 'filterByLength']);
+Route::get('quotes/popular', [QuotesController::class, 'popular']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
